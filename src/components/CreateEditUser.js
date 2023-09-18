b7b8f5b936d54a9895d9a044891abdfcd44849ac
harsh <harshgami0509@gmail.com>
2023-09-18 21:12:06 +0530

    final touch

--- src/components/CreateEditUser.js
+++ src/components/CreateEditUser.js
@@ -379,6 +379,108 @@ function CreateEditUser() {
 
                                 <div className="sm:col-span-2">
                                   <label
+                                    htmlFor="country"
+                                    className="block text-base font-medium leading-6 text-gray-900"
+                                  >
+                                    Country*
+                                  </label>
+                                  <Combobox
+                                    value={selectedCountry.name}
+                                    onChange={setSelectedCountry}
+                                  >
+                                    <div className="relative mt-2">
+                                      <div className="relative w-full border-2 cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+                                        <Combobox.Input
+                                          className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                                          displayValue={selectedCountry.name}
+                                          onChange={(event) =>
+                                            setCountryQuery(event.target.value)
+                                          }
+                                        />
+                                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                                          <ChevronUpDownIcon
+                                            className="h-5 w-5 text-gray-400"
+                                            aria-hidden="true"
+                                          />
+                                        </Combobox.Button>
+                                      </div>
+                                      <Transition
+                                        as={Fragment}
+                                        leave="transition ease-in duration-200"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                        afterLeave={() => setCountryQuery("")}
+                                      >
+                                        <Combobox.Options className="absolute mt-1 max-h-28 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                          {filteredcountryData.length === 0 &&
+                                          countryQuery !== "" ? (
+                                            <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                                              Nothing found.
+                                            </div>
+                                          ) : (
+                                            filteredcountryData.map(
+                                              (country, index) => (
+                                                <Combobox.Option
+                                                  key={index}
+                                                  className={({ active }) =>
+                                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                                      active
+                                                        ? "bg-teal-600 text-white"
+                                                        : "text-gray-900"
+                                                    }`
+                                                  }
+                                                  value={{
+                                                    name: country.name,
+                                                    code: country.isoCode,
+                                                  }}
+                                                >
+                                                  {({
+                                                    selectedCountry,
+                                                    active,
+                                                  }) => (
+                                                    <>
+                                                      <span
+                                                        className={`block truncate ${
+                                                          selectedCountry
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                        }`}
+                                                      >
+                                                        {country.name}
+                                                      </span>
+                                                      {selectedCountry ? (
+                                                        <span
+                                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                            active
+                                                              ? "text-white"
+                                                              : "text-teal-600"
+                                                          }`}
+                                                        >
+                                                          <CheckIcon
+                                                            className="h-5 w-5"
+                                                            aria-hidden="true"
+                                                          />
+                                                        </span>
+                                                      ) : null}
+                                                    </>
+                                                  )}
+                                                </Combobox.Option>
+                                              )
+                                            )
+                                          )}
+                                        </Combobox.Options>
+                                      </Transition>
+                                    </div>
+                                  </Combobox>
+                                  {selectedCountry.name === "" ? (
+                                    <span className="mt-2 text-sm text-red-500">
+                                      Please select country
+                                    </span>
+                                  ) : null}
+                                </div>
+
+                                <div className="sm:col-span-2">
+                                  <label
                                     htmlFor="region"
                                     className="block text-base font-medium leading-6 text-gray-900"
                                   >
@@ -491,107 +593,6 @@ function CreateEditUser() {
                                   )}
                                 </div>
 
-                                <div className="sm:col-span-2">
-                                  <label
-                                    htmlFor="country"
-                                    className="block text-base font-medium leading-6 text-gray-900"
-                                  >
-                                    Country*
-                                  </label>
-                                  <Combobox
-                                    value={selectedCountry.name}
-                                    onChange={setSelectedCountry}
-                                  >
-                                    <div className="relative mt-2">
-                                      <div className="relative w-full border-2 cursor-default overflow-hidden rounded-lg bg-white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                                        <Combobox.Input
-                                          className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                                          displayValue={selectedCountry.name}
-                                          onChange={(event) =>
-                                            setCountryQuery(event.target.value)
-                                          }
-                                        />
-                                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                                          <ChevronUpDownIcon
-                                            className="h-5 w-5 text-gray-400"
-                                            aria-hidden="true"
-                                          />
-                                        </Combobox.Button>
-                                      </div>
-                                      <Transition
-                                        as={Fragment}
-                                        leave="transition ease-in duration-200"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                        afterLeave={() => setCountryQuery("")}
-                                      >
-                                        <Combobox.Options className="absolute mt-1 max-h-28 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                          {filteredcountryData.length === 0 &&
-                                          countryQuery !== "" ? (
-                                            <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                                              Nothing found.
-                                            </div>
-                                          ) : (
-                                            filteredcountryData.map(
-                                              (country, index) => (
-                                                <Combobox.Option
-                                                  key={index}
-                                                  className={({ active }) =>
-                                                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                      active
-                                                        ? "bg-teal-600 text-white"
-                                                        : "text-gray-900"
-                                                    }`
-                                                  }
-                                                  value={{
-                                                    name: country.name,
-                                                    code: country.isoCode,
-                                                  }}
-                                                >
-                                                  {({
-                                                    selectedCountry,
-                                                    active,
-                                                  }) => (
-                                                    <>
-                                                      <span
-                                                        className={`block truncate ${
-                                                          selectedCountry
-                                                            ? "font-medium"
-                                                            : "font-normal"
-                                                        }`}
-                                                      >
-                                                        {country.name}
-                                                      </span>
-                                                      {selectedCountry ? (
-                                                        <span
-                                                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                                            active
-                                                              ? "text-white"
-                                                              : "text-teal-600"
-                                                          }`}
-                                                        >
-                                                          <CheckIcon
-                                                            className="h-5 w-5"
-                                                            aria-hidden="true"
-                                                          />
-                                                        </span>
-                                                      ) : null}
-                                                    </>
-                                                  )}
-                                                </Combobox.Option>
-                                              )
-                                            )
-                                          )}
-                                        </Combobox.Options>
-                                      </Transition>
-                                    </div>
-                                  </Combobox>
-                                  {selectedCountry.name === "" ? (
-                                    <span className="mt-2 text-sm text-red-500">
-                                      Please select country
-                                    </span>
-                                  ) : null}
-                                </div>
 
                                 <div className="sm:col-span-2">
                                   <label
